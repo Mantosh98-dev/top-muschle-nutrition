75049e36b3c923fc15830f415a9d62119b4a7380
Mantosh98-dev <mantosh9811@gmail.com>
2026-07-12 15:06:49 +0530

    Delete all existing code

--- js/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-  SUPABASE_URL: "https://hblbnsgwrjmpgjmmhpoy.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhibGJuc2d3cmptcGdqbW1ocG95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3NzA5MDAsImV4cCI6MjA5ODM0NjkwMH0.cXNnCgaHE7NI85g6a9PZy3yvbsF3uq6JDhSs11zJPVg"
-};
-
-export default CONFIG;
